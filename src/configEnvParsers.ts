8d19609742b29dcb3c297c121fc5d901d6f2f82c
Jakub Švehla <svehl.jakub@gmail.com>
2021-03-19 15:53:19 +0100

    increase version

--- src/configEnvParsers.ts
+++ src/configEnvParsers.ts
@@ -73,7 +73,7 @@ export const getListFromEnvParser = <T = string>(
 const isBrowser = typeof window === 'object'
 
 export const getEnvFromFileParser: (env: string, required?: boolean) => () => string = isBrowser
-  ? () => () => {
+  ? () => {
       throw new Error(`You cant import 'getEnvFromFileParser' in the browser`)
     }
   : // eslint-disable-next-line @typescript-eslint/no-var-requires

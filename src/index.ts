ab039822ddd539c9cfccc3e47fb577ecb8d00eec
Jakub Svehla <svehl.jakub@gmail.com>
2024-10-09 12:02:31 +0200

    add auto lint + window support

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+if (typeof process === 'undefined') {
+  // make it compatible for FE environments
+  process = window.process
+}
+
 export * from './configEnvParsers'
 export * from './validateConfig'
 export * from './ValidationError'
